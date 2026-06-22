9d1fff5567bd2ce8ea17ac8cbc67d98d84104a34
Yan-serra <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>
2026-06-22 11:36:42 -0300

    back-end

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from database import engine, SessionLocal, Base
+from models import User
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"mensagem": "CloudTask AI SaaS"}
+#CREATE ITEM
+@app.post("/users")
+def create_item(name: str, age: int, email: str):
+    db = SessionLocal()
+    user = User(name=name, age=age, email=email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+#LIST ITEMS
+@app.get("/users")
+def list_users():
+    db = SessionLocal()
+    return db.query(User).all()
+
+#DELETE ITEM
+@app.delete("/users/{id}")
+def delete_item(id: int):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == id).first()
+    db.delete(user)
+    db.commit()
+    return {"message": "Usuario deletado com sucesso!"}
